dd28f70e11ee69c56090158dcb43d6e48e489473
master <master@A33PC15>
2023-03-13 19:27:25 +0200

    Init

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "./containers";
+
+const App: React.FC = () => {
+  return <DashboardLayout />;
+};
+
+export default App;
---
+++ dashboard/src/containers/dashboardLayout/listItems/index.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <Link to="">
+        <ListItemText primary="Dashboard" />
+      </Link>
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <Link to="users">
+        <ListItemText primary="Users" />
+      </Link>
+    </ListItemButton>
+  </React.Fragment>
+);
+
+export const secondaryListItems = (
+  <React.Fragment>
+    <ListSubheader component="div" inset>
+      Features
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItemButton>
+  </React.Fragment>
+);
