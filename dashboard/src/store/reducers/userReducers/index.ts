4d50010c5259607b361165ad801e1a6ab32a6078
master <master@A33PC15>
2023-03-15 20:22:24 +0200

    Add table

--- dashboard/src/store/reducers/userReducers/index.ts
+++ dashboard/src/store/reducers/userReducers/index.ts
@@ -1,22 +1,50 @@
 import { UserState, UserActions, UserActionType } from "./types";
 
 const initialState: UserState = {
-    allUsers:[{
-        id: 1,
-        name: "Bill",
-        surname: "Smith"
-    }],
-    loading: false
-}
+  allUsers: [
+    {
+      id: 1,
+      name: "Bill",
+      surname: "Smith",
+    },
+
+    {
+      id: 2,
+      name: "Eva",
+      surname: "Andesen",
+    },
+    {
+      id: 3,
+      name: "Stiven",
+      surname: "Sigal",
+    },
+    {
+      id: 4,
+      name: "Rebert",
+      surname: "Signal",
+    },
+    {
+      id: 5,
+      name: "Camilla",
+      surname: "Tomson",
+    },
+    {
+      id: 6,
+      name: "Evan",
+      surname: "Roberts",
+    },
+  ],
+  loading: false,
+};
 const UserReducer = (state = initialState, action: UserActions): UserState => {
-    switch(action.type){
-        case UserActionType.START_REQUEST:
-            return { ...state, loading: true }
-        case UserActionType.ALL_USERS_LOADED:
-            return { ...state, loading: false, allUsers: action.payload }    
-        default: 
-        return state
-    }
-}
+  switch (action.type) {
+    case UserActionType.START_REQUEST:
+      return { ...state, loading: true };
+    case UserActionType.ALL_USERS_LOADED:
+      return { ...state, loading: false, allUsers: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default UserReducer;
