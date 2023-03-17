b7c955d1d1a18986fce09cb59f0a6b747eea2838
master <master@A33PC15>
2023-03-17 18:38:08 +0200

    Forms

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -3,14 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import DashboardLayout from "./containers";
 import DefaultPage from "./pages/defaultPage";
 import Users from "./pages/users";
+import NotFound from "./pages/notFound";
+import SignIn from "./pages/singIn";
+import SignUp from "./pages/signUp";
 
 const App: React.FC = () => {
   return (
     <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
       <Route path="/dashboard" element={<DashboardLayout />}>
         <Route index element={<DefaultPage />} />
         <Route path="users" element={<Users />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
